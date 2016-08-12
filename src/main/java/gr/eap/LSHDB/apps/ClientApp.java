afd16d1cd357c561608668f742af9aa1a15e67d5
Dimitrios Karapiperis <karapiperis121@gmail.com>
2016-08-12 23:36:47 +0300

    initial commit

---
+++ src/main/java/gr/eap/LSHDB/apps/ClientApp.java
@@ -0,0 +1,55 @@
+package gr.eap.LSHDB.apps;
+
+
+import gr.eap.LSHDB.HammingLSHStore;
+import gr.eap.LSHDB.StoreInitException;
+import gr.eap.LSHDB.util.QueryRecord;
+import gr.eap.LSHDB.util.Record;
+import gr.eap.LSHDB.util.Result;
+import java.util.ArrayList;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author dimkar
+ */
+public class ClientApp{
+
+    HammingLSHStore lsh;
+    String storeName = "dblp";
+
+    public ClientApp() {        
+       try{ 
+        String folder = "c:/MAPDB";
+        String engine = "gr.eap.LSHDB.MapDB";
+        lsh = new HammingLSHStore(folder, storeName, engine);
+       }catch(StoreInitException ex){
+           System.out.println(ex.getMessage());
+           System.exit(0);
+       } 
+    }
+
+    public void query() {
+        QueryRecord query = new QueryRecord(40);
+        query.setKeyedField("author", new String[]{"John"},.8,true);
+        Result r = lsh.query(query);
+        r.prepare();  
+        ArrayList<Record> arr= r.getRecords();
+        for (int i=0;i<arr.size();i++){
+            Record rec = arr.get(i);
+            System.out.println(rec.get("author")+" "+rec.get("title"));
+        }
+        lsh.close();
+    }
+
+    public static void main(String[] args) {
+        ClientApp app = new ClientApp();
+        app.query();        
+     }
+
+}
