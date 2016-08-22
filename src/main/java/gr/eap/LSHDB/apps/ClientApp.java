69e769056e4f923598e07a07129f580d5f3df6cf
dimkar121 <karapiperis121@gmail.com>
2016-08-22 09:38:47 +0300

    a

--- src/main/java/gr/eap/LSHDB/apps/ClientApp.java
+++ src/main/java/gr/eap/LSHDB/apps/ClientApp.java
@@ -11,12 +11,16 @@ import gr.eap.LSHDB.client.Client;
 import gr.eap.LSHDB.util.QueryRecord;
 import gr.eap.LSHDB.util.Record;
 import gr.eap.LSHDB.util.Result;
-import java.awt.List;
 import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.ObjectInput;
 import java.io.ObjectInputStream;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
 import java.lang.management.ManagementFactory;
 import java.lang.management.ThreadMXBean;
 import java.util.ArrayList;
@@ -35,47 +39,54 @@ public class ClientApp {
     HammingLSHStore lsh;
     String storeName = "dblp";
 
-    public ClientApp() {
-        try {
-            String folder = "c:/MAPDB";
-            String engine = "gr.eap.LSHDB.MapDB";
-            //lsh = new HammingLSHStore(folder, storeName, engine);
-            //HammingKey key=(HammingKey) lsh.getConfiguration().getKey("author");
-            //int[][] samples = key.samples;
-            int[][] samples=null;
-            try{
-                InputStream file = new FileInputStream("c:/Users/dimkar/samples.ser");
-                InputStream buffer = new BufferedInputStream(file);
-                ObjectInput input = new ObjectInputStream(buffer);
-                samples = (int[][])  input.readObject();
+    
+    public int[][] deser(){
+         try{
+             InputStream file = new FileInputStream("c:/Users/dimkar/samples.ser");
+             InputStream buffer = new BufferedInputStream(file);
+             ObjectInput input = new ObjectInputStream(buffer);
+             int[][] samples = (int[][])  input.readObject();
+             return samples;
              }catch(Exception ex){
-                 ex.printStackTrace();
-             }   
-            HammingKey key=new HammingKey("author");
-            key.samples = samples;
-            
-            HammingConfiguration hc = new HammingConfiguration(folder, storeName, engine, new Key[]{key}, true);
-            hc.saveConfiguration();
-             /* try {
+             ex.printStackTrace();
+             } 
+        return null; 
+    }
+    
+    
+    
+    public void ser(int[][] samples){
+         try {
                 OutputStream file = new FileOutputStream("c:/Users/dimkar/samples.ser");
                 OutputStream buffer = new BufferedOutputStream(file);
                 ObjectOutput output = new ObjectOutputStream(buffer);
                 output.writeObject(samples);
                 output.flush();
                 output.close();
-             }catch(Exception ex){
-                 ex.printStackTrace();
-             }*/
-             
-             
-             //HammingKey newKey = new HammingKey("author");
-            //newKey.samples = samples;
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+    }
+    
+    
+    public ClientApp() {
+        try {
+            String folder = "c:/MAPDB";
+            String engine = "gr.eap.LSHDB.MapDB";
+            lsh = new HammingLSHStore(folder, storeName, engine);
+            HammingKey key = (HammingKey) lsh.getConfiguration().getKey("author");
+            int[][] samples = key.samples;
+            ser(samples);
             
             
             
-            
-            
+            //int[][] samples=deser();
+            //HammingKey key=new HammingKey("author");
+            //key.samples = deser();
+            //HammingConfiguration hc = new HammingConfiguration(folder, storeName, engine, new Key[]{key}, true);
+            //hc.saveConfiguration();
            
+
         } catch (StoreInitException ex) {
             System.out.println(ex.getMessage());
             System.exit(0);
@@ -109,23 +120,23 @@ public class ClientApp {
             int a = i;
             threads[i] = new Thread(new Runnable() {
                 public void run() {
-                  for (int j=0;j<1000;j++){  
-                    try {
-                        Thread.sleep(200);
-                        ThreadMXBean bean = ManagementFactory.getThreadMXBean();
-                        int t = bean.getThreadCount();
-                        //System.out.println("t="+t);    
-                        QueryRecord query = new QueryRecord("dblp", 100);
-                        query.setKeyedField("author", new String[]{s}, .8, true);
-                        final Client c = new Client("localhost", 4443);
-                        Result r = c.queryServer(query);
-                        System.out.println(a + ".  " + r.getRecords().size() + " " + r.getTime());
-                    } catch (NodeCommunicationException ex) {
-                        System.out.println(ex.getMessage());
-                    } catch (InterruptedException ex) {
-                        System.out.println(ex.getMessage());
+                    for (int j = 0; j < 1000; j++) {
+                        try {
+                            Thread.sleep(200);
+                            ThreadMXBean bean = ManagementFactory.getThreadMXBean();
+                            int t = bean.getThreadCount();
+                            //System.out.println("t="+t);    
+                            QueryRecord query = new QueryRecord("dblp", 100);
+                            query.setKeyedField("author", new String[]{s}, .8, true);
+                            final Client c = new Client("localhost", 4443);
+                            Result r = c.queryServer(query);
+                            System.out.println(a + ".  " + r.getRecords().size() + " " + r.getTime());
+                        } catch (NodeCommunicationException ex) {
+                            System.out.println(ex.getMessage());
+                        } catch (InterruptedException ex) {
+                            System.out.println(ex.getMessage());
+                        }
                     }
-                 }
                 }
             });
             threads[i].start();
